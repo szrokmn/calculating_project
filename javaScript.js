288c9ad8098bd23d96cd0486eabb457f42753d65
Sezer Ökmen <szrokmn34@gmail.com>
2022-09-11 16:07:57 +0300

    js finished

--- javaScript.js
+++ javaScript.js
@@ -8,3 +8,40 @@ function topla() {
     document.getElementById("sonuc").innerHTML = sayi3.toFixed(3);
    
    }
+   function cikar() {
+
+    let  sayi1 = document.getElementById("input1").value;
+    let  sayi2 = document.getElementById("input2").value;
+  
+      let sayi3 = parseFloat(sayi1) - parseFloat(sayi2);
+  
+      document.getElementById("sonuc").innerHTML =
+  
+      sayi3.toFixed(3);
+  }
+  
+  
+  function carp() {
+  
+    let  sayi1 = document.getElementById("input1").value;
+    let  sayi2 = document.getElementById("input2").value;
+  
+      let sayi3 = parseFloat(sayi1) * parseFloat(sayi2);
+  
+      document.getElementById("sonuc").innerHTML =
+  
+      sayi3.toFixed(3);
+  }
+  
+  
+  function bol() {
+  
+    let  sayi1 = document.getElementById("input1").value;
+    let  sayi2 = document.getElementById("input2").value;
+  
+      let sayi3 = parseFloat(sayi1) / parseFloat(sayi2);
+  
+      document.getElementById("sonuc").innerHTML =
+  
+      sayi3.toFixed(3);
+  }
